0bfa97f372e4239dbaadef392cc7dbfa848658d5
MuhammadNauman <m.nauman332@gmail.com>
2019-07-18 11:21:45 +0500

    hello world

--- app/src/main/java/com/khan/serviceprovider/Main2Activity.java
+++ app/src/main/java/com/khan/serviceprovider/Main2Activity.java
@@ -3,13 +3,12 @@ package com.khan.serviceprovider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        int a;
     }
 }
