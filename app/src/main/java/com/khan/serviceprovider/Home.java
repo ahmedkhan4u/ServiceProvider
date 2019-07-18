7fae443a9b940ea8823a30ede714112113961d47
cjnomi <m.nauman332@gmail.com>
2019-07-18 15:45:13 +0500

    updated4 with the json file of firebase and login and registratiton system

--- app/src/main/java/com/khan/serviceprovider/Home.java
+++ app/src/main/java/com/khan/serviceprovider/Home.java
@@ -3,12 +3,12 @@ package com.khan.serviceprovider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_home);
 
     }
 }
