012d8970ae7541f855cd662357aca7fb18589907
cjnomi <m.nauman332@gmail.com>
2019-07-20 16:26:19 +0500

    updated4 with the json file of firebase and login and registratiton system

--- app/src/main/java/com/khan/serviceprovider/Home.java
+++ app/src/main/java/com/khan/serviceprovider/Home.java
@@ -1,71 +1,69 @@
 package com.khan.serviceprovider;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class Home extends AppCompatActivity {
-
-    Toolbar toolbar;
-    FirebaseAuth firebaseAuth;
-
+public class Home extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
-            case R.id.home:
-            {
-                return true;
-            }
-            case R.id.profile:
-            {
-                return true;
-            }
-            case R.id.rate_us:
-            {
-                return true;
-            }
-            case R.id.about:
-            {
-                return true;
-            }
-            case R.id.logout:
-            {
-                firebaseAuth.signOut();
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                return true;
-            }
-            default:
-            {
-                return super.onOptionsItemSelected(item);
-            }
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_profile) {
+
+        } else if (id == R.id.nav_logout) {
+
+        } else if (id == R.id.nav_rate) {
+
+        } else if (id == R.id.nav_about) {
+
         }
 
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.toolbar_menu,menu);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 }
