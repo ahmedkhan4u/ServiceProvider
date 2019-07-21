4b54f4ac8c2c6d873439fea60bab8714b070efe4
cjnomi <m.nauman332@gmail.com>
2019-07-21 13:10:06 +0500

    fragment updated + floating

--- app/src/main/java/com/khan/serviceprovider/FragmentHome.java
+++ app/src/main/java/com/khan/serviceprovider/FragmentHome.java
@@ -3,10 +3,12 @@ package com.khan.serviceprovider;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class FragmentHome extends Fragment {
 
@@ -14,6 +16,13 @@ public class FragmentHome extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
+        FloatingActionButton floatingActionButton = (FloatingActionButton) view.findViewById(R.id.floatingaction);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 }
